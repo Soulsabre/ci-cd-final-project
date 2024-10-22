4be360d0fe5230ed3856cc35b8892d9eaf5b112e
Schawn Statham <sstatham.dev@outlook.com>
2024-10-22 00:54:01 -0400

    More indentation fixes for routes.py

--- service/routes.py
+++ service/routes.py
@@ -56,15 +56,15 @@ def create_counters(name):
 
     if name in COUNTER:
         return abort(status.HTTP_409_CONFLICT,
-                        f"Counter {name} already exists")
+                    f"Counter {name} already exists")
 
     COUNTER[name] = 0
 
     location_url = url_for("read_counters", name=name, _external=True)
     return (
         jsonify(name=name, counter=0),
-            status.HTTP_201_CREATED,
-            {"Location": location_url},
+                status.HTTP_201_CREATED,
+                {"Location": location_url},
     )
 
 
@@ -78,7 +78,7 @@ def read_counters(name):
 
     if name not in COUNTER:
         return abort(status.HTTP_404_NOT_FOUND,
-                        f"Counter {name} does not exist")
+                    f"Counter {name} does not exist")
 
     counter = COUNTER[name]
     return jsonify(name=name, counter=counter)
@@ -94,7 +94,7 @@ def update_counters(name):
 
     if name not in COUNTER:
         return abort(status.HTTP_404_NOT_FOUND,
-                        f"Counter {name} does not exist")
+                    f"Counter {name} does not exist")
 
     COUNTER[name] += 1
 
